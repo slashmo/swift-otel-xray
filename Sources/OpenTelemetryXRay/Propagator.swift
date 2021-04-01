33ff17e287a6f09afdd0d40cd5465200a26c6ddd
Moritz Lang <hi@slashmo.codes>
2021-04-01 11:21:30 +0200

    Add XRayPropagator

---
+++ Sources/OpenTelemetryXRay/Propagator.swift
@@ -0,0 +1,154 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift OpenTelemetry open source project
+//
+// Copyright (c) 2021 Moritz Lang and the Swift OpenTelemetry project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Instrumentation
+@_exported import OpenTelemetry
+
+/// An `OTelPropagator` that propagates span context through the `X-Amzn-Trace-Id` header.
+///
+/// - SeeAlso: [AWS X-Ray: Tracing Header](https://docs.aws.amazon.com/xray/latest/devguide/xray-concepts.html#xray-concepts-tracingheader)
+public struct XRayPropagator: OTelPropagator {
+    private static let tracingHeader = "X-Amzn-Trace-Id"
+
+    /// Initialize an X-Ray compatible propagator.
+    public init() {}
+
+    public func inject<Carrier, Inject>(
+        _ spanContext: OTel.SpanContext,
+        into carrier: inout Carrier,
+        using injector: Inject
+    ) where Inject: Injector, Carrier == Inject.Carrier {
+        let traceIDBytes = spanContext.traceID.hexBytes
+        let timestampBytes = traceIDBytes[0 ..< 8]
+        let randomBytes = traceIDBytes[8...]
+
+        let tracingHeader = [
+            "Root=1-\(String(decoding: timestampBytes, as: UTF8.self))-\(String(decoding: randomBytes, as: UTF8.self))",
+            "Parent=\(spanContext.spanID)",
+            "Sampled=\(spanContext.traceFlags.contains(.sampled) ? "1" : "0")",
+        ].joined(separator: ";")
+
+        injector.inject(tracingHeader, forKey: Self.tracingHeader, into: &carrier)
+    }
+
+    public func extractSpanContext<Carrier, Extract>(
+        from carrier: Carrier,
+        using extractor: Extract
+    ) throws -> OTel.SpanContext? where Extract: Extractor, Carrier == Extract.Carrier {
+        guard let tracingHeader = extractor.extract(key: Self.tracingHeader, from: carrier) else {
+            return nil
+        }
+
+        var extractedTraceID: OTel.TraceID?
+        var spanID: OTel.SpanID?
+        var extractedTraceFlags: OTel.TraceFlags?
+
+        let parts = tracingHeader.split(separator: ";")
+        var iterator = parts.makeIterator()
+
+        while let part = iterator.next() {
+            if part.starts(with: "Root=") {
+                let startIndex = part.index(part.startIndex, offsetBy: 5)
+                extractedTraceID = try extractTraceID(part[startIndex...])
+            } else if part.starts(with: "Parent=") {
+                let startIndex = part.index(part.startIndex, offsetBy: 7)
+                spanID = try extractSpanID(part[startIndex...])
+            } else if part.starts(with: "Sampled=") {
+                let startIndex = part.index(part.startIndex, offsetBy: 8)
+                let sampledString = part[startIndex...]
+                extractedTraceFlags = sampledString == "1" ? .sampled : []
+            }
+        }
+
+        guard let traceID = extractedTraceID else {
+            throw TraceHeaderParsingError(value: tracingHeader, reason: .missingTraceID)
+        }
+        guard let traceFlags = extractedTraceFlags else {
+            throw TraceHeaderParsingError(value: tracingHeader, reason: .missingSampleDecision)
+        }
+
+        return OTel.SpanContext(
+            traceID: traceID,
+            spanID: spanID ?? OTel.SpanID(bytes: (0, 0, 0, 0, 0, 0, 0, 0)),
+            traceFlags: traceFlags,
+            isRemote: true
+        )
+    }
+
+    private func extractTraceID<S: StringProtocol>(_ string: S) throws -> OTel.TraceID {
+        let result = try string.utf8.withContiguousStorageIfAvailable { traceIDBytes -> OTel.TraceID in
+            guard traceIDBytes.count == 35 else {
+                throw TraceHeaderParsingError(value: String(string), reason: .invalidTraceIDLength(string.count))
+            }
+
+            guard traceIDBytes[0] == UInt8(ascii: "1") else {
+                throw TraceHeaderParsingError(
+                    value: String(string),
+                    reason: .unsupportedTraceIDVersion(String(string[string.startIndex]))
+                )
+            }
+
+            guard traceIDBytes[1] == UInt8(ascii: "-"), traceIDBytes[10] == UInt8(ascii: "-") else {
+                throw TraceHeaderParsingError(value: String(string), reason: .invalidTraceIDDelimiters)
+            }
+
+            var traceIDStorage: OTel.TraceID.Bytes = (
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            )
+            withUnsafeMutableBytes(of: &traceIDStorage) { ptr in
+                let timestampRegion = UnsafeMutableRawBufferPointer(rebasing: ptr[0 ..< 4])
+                let randomRegion = UnsafeMutableRawBufferPointer(rebasing: ptr[4...])
+                OTel.Hex.convert(traceIDBytes[2 ..< 10], toBytes: timestampRegion)
+                OTel.Hex.convert(traceIDBytes[11 ..< 35], toBytes: randomRegion)
+            }
+
+            return OTel.TraceID(bytes: traceIDStorage)
+        }
+        return try result ?? extractTraceID(String(string))
+    }
+
+    private func extractSpanID<S: StringProtocol>(_ string: S) throws -> OTel.SpanID {
+        let result = try string.utf8.withContiguousStorageIfAvailable { spanIDBytes -> OTel.SpanID in
+            guard spanIDBytes.count == 16 else {
+                throw TraceHeaderParsingError(value: String(string), reason: .invalidSpanIDLength(spanIDBytes.count))
+            }
+
+            var bytes: OTel.SpanID.Bytes = (0, 0, 0, 0, 0, 0, 0, 0)
+            withUnsafeMutableBytes(of: &bytes) { ptr in
+                OTel.Hex.convert(spanIDBytes, toBytes: ptr)
+            }
+            return OTel.SpanID(bytes: bytes)
+        }
+        return try result ?? extractSpanID(String(string))
+    }
+}
+
+extension XRayPropagator {
+    public struct TraceHeaderParsingError: Error, Equatable {
+        public let value: String
+        public let reason: Reason
+    }
+}
+
+extension XRayPropagator.TraceHeaderParsingError {
+    public enum Reason: Equatable {
+        case missingTraceID
+        case invalidTraceIDLength(Int)
+        case unsupportedTraceIDVersion(String)
+        case invalidTraceIDDelimiters
+
+        case invalidSpanIDLength(Int)
+
+        case missingSampleDecision
+    }
+}
---
+++ Tests/OpenTelemetryXRayTests/PropagatorTests.swift
@@ -0,0 +1,209 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift OpenTelemetry open source project
+//
+// Copyright (c) 2021 Moritz Lang and the Swift OpenTelemetry project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Instrumentation
+@testable import OpenTelemetryXRay
+import XCTest
+
+final class XRayPropagatorTests: XCTestCase {
+    private let propagator = XRayPropagator()
+    private let injector = DictionaryInjector()
+    private let extractor = DictionaryExtractor()
+
+    // MARK: - Inject
+
+    func test_injectsTraceparentHeader_notSampled() {
+        let spanContext = OTel.SpanContext(
+            traceID: OTel.TraceID(bytes: (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)),
+            spanID: OTel.SpanID(bytes: (1, 2, 3, 4, 5, 6, 7, 8)),
+            traceFlags: [],
+            isRemote: false
+        )
+        var headers = [String: String]()
+
+        propagator.inject(spanContext, into: &headers, using: injector)
+
+        XCTAssertEqual(
+            headers,
+            ["X-Amzn-Trace-Id": "Root=1-01020304-05060708090a0b0c0d0e0f10;Parent=0102030405060708;Sampled=0"]
+        )
+    }
+
+    func test_injectsTraceparentHeader_sampled() {
+        let spanContext = OTel.SpanContext(
+            traceID: OTel.TraceID(bytes: (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)),
+            spanID: OTel.SpanID(bytes: (1, 2, 3, 4, 5, 6, 7, 8)),
+            traceFlags: .sampled,
+            isRemote: false
+        )
+        var headers = [String: String]()
+
+        propagator.inject(spanContext, into: &headers, using: injector)
+
+        XCTAssertEqual(
+            headers,
+            ["X-Amzn-Trace-Id": "Root=1-01020304-05060708090a0b0c0d0e0f10;Parent=0102030405060708;Sampled=1"]
+        )
+    }
+
+    // MARK: - Extract
+
+    func test_extractsNil_withoutTracingHeader() throws {
+        let headers = ["Content-Type": "application/json"]
+
+        XCTAssertNil(try propagator.extractSpanContext(from: headers, using: extractor))
+    }
+
+    func test_extractsTracingHeader_notSampled() throws {
+        let headers = [
+            "X-Amzn-Trace-Id": "Root=1-5759e988-bd862e3fe1be46a994272793;Sampled=0",
+        ]
+
+        let spanContext = try XCTUnwrap(propagator.extractSpanContext(from: headers, using: extractor))
+
+        XCTAssertEqual(spanContext.traceID.description, "5759e988bd862e3fe1be46a994272793")
+        XCTAssertEqual(spanContext.spanID.description, "0000000000000000")
+        XCTAssertTrue(spanContext.traceFlags.isEmpty)
+        XCTAssertNil(spanContext.traceState)
+    }
+
+    func test_extractsTracingHeader_sampled() throws {
+        let headers = [
+            "X-Amzn-Trace-Id": "Root=1-5759e988-bd862e3fe1be46a994272793;Sampled=1",
+        ]
+
+        let spanContext = try XCTUnwrap(propagator.extractSpanContext(from: headers, using: extractor))
+
+        XCTAssertEqual(spanContext.traceID.description, "5759e988bd862e3fe1be46a994272793")
+        XCTAssertEqual(spanContext.spanID.description, "0000000000000000")
+        XCTAssertEqual(spanContext.traceFlags, .sampled)
+        XCTAssertNil(spanContext.traceState)
+    }
+
+    func test_extractsTracingHeader_notSampled_withParent() throws {
+        let headers = [
+            "X-Amzn-Trace-Id": "Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Sampled=0",
+        ]
+
+        let spanContext = try XCTUnwrap(propagator.extractSpanContext(from: headers, using: extractor))
+
+        XCTAssertEqual(spanContext.traceID.description, "5759e988bd862e3fe1be46a994272793")
+        XCTAssertEqual(spanContext.spanID.description, "53995c3f42cd8ad8")
+        XCTAssertTrue(spanContext.traceFlags.isEmpty)
+        XCTAssertNil(spanContext.traceState)
+    }
+
+    func test_extractsTracingHeader_sampled_withParent() throws {
+        let headers = [
+            "X-Amzn-Trace-Id": "Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Sampled=1",
+        ]
+
+        let spanContext = try XCTUnwrap(propagator.extractSpanContext(from: headers, using: extractor))
+
+        XCTAssertEqual(spanContext.traceID.description, "5759e988bd862e3fe1be46a994272793")
+        XCTAssertEqual(spanContext.spanID.description, "53995c3f42cd8ad8")
+        XCTAssertEqual(spanContext.traceFlags, .sampled)
+        XCTAssertNil(spanContext.traceState)
+    }
+
+    func test_extractFails_missingTraceID() throws {
+        let headers = ["X-Amzn-Trace-Id": "Root=1-123-456;Sampled=1"]
+
+        XCTAssertThrowsError(
+            try propagator.extractSpanContext(from: headers, using: extractor),
+            XRayPropagator.TraceHeaderParsingError(value: "1-123-456", reason: .invalidTraceIDLength(9))
+        )
+    }
+
+    func test_extractFails_missingSampleDecision() throws {
+        let tracingHeader = "Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8"
+        let headers = ["X-Amzn-Trace-Id": tracingHeader]
+
+        XCTAssertThrowsError(
+            try propagator.extractSpanContext(from: headers, using: extractor),
+            XRayPropagator.TraceHeaderParsingError(value: tracingHeader, reason: .missingSampleDecision)
+        )
+    }
+
+    func test_extractFails_invalidTraceIDLength() throws {
+        let tracingHeader = "Parent=53995c3f42cd8ad8;Sampled=1"
+        let headers = ["X-Amzn-Trace-Id": tracingHeader]
+
+        XCTAssertThrowsError(
+            try propagator.extractSpanContext(from: headers, using: extractor),
+            XRayPropagator.TraceHeaderParsingError(value: tracingHeader, reason: .missingTraceID)
+        )
+    }
+
+    func test_extractFails_unsupportedTraceIDVersion() throws {
+        let traceID = "2-5759e988-bd862e3fe1be46a994272793"
+        let headers = ["X-Amzn-Trace-Id": "Root=\(traceID);Sampled=1"]
+
+        XCTAssertThrowsError(
+            try propagator.extractSpanContext(from: headers, using: extractor),
+            XRayPropagator.TraceHeaderParsingError(value: traceID, reason: .unsupportedTraceIDVersion("2"))
+        )
+    }
+
+    func test_extractFails_invalidTraceIDDelimiters() throws {
+        let traceID = "1_5759e988*bd862e3fe1be46a994272793"
+        let headers = ["X-Amzn-Trace-Id": "Root=\(traceID);Sampled=1"]
+
+        XCTAssertThrowsError(
+            try propagator.extractSpanContext(from: headers, using: extractor),
+            XRayPropagator.TraceHeaderParsingError(value: traceID, reason: .invalidTraceIDDelimiters)
+        )
+    }
+
+    func test_extractFails_invalidSpanIDLength() throws {
+        let tracingHeader = "Root=1-5759e988-bd862e3fe1be46a994272793;Parent=short;Sampled=1"
+        let headers = ["X-Amzn-Trace-Id": tracingHeader]
+
+        XCTAssertThrowsError(
+            try propagator.extractSpanContext(from: headers, using: extractor),
+            XRayPropagator.TraceHeaderParsingError(value: "short", reason: .invalidSpanIDLength(5))
+        )
+    }
+
+    // MARK: - End To End
+
+    func test_injectExtractedSpanContext() throws {
+        let headers = [
+            "X-Amzn-Trace-Id": "Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Sampled=1",
+        ]
+
+        let extractedSpanContext = try XCTUnwrap(propagator.extractSpanContext(from: headers, using: extractor))
+
+        var injectedHeaders = [String: String]()
+
+        propagator.inject(extractedSpanContext, into: &injectedHeaders, using: injector)
+
+        XCTAssertEqual(injectedHeaders["X-Amzn-Trace-Id"], headers["X-Amzn-Trace-Id"])
+    }
+}
+
+private struct DictionaryInjector: Injector {
+    init() {}
+
+    func inject(_ value: String, forKey key: String, into carrier: inout [String: String]) {
+        carrier[key] = value
+    }
+}
+
+private struct DictionaryExtractor: Extractor {
+    init() {}
+
+    func extract(key: String, from carrier: [String: String]) -> String? {
+        carrier[key]
+    }
+}

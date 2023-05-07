777a5c2f6b014184bbb79ac41e2a2e91584dd5d0
tachyonics <simon@tachyonics.io>
2023-05-07 17:21:25 +1000

    Update to use apple/swift-distributed-tracing 1.0.0-beta.2. (#10)
    * Update to use apple/swift-distributed-tracing 1.0.0-beta.2.
    
    * [CI] Run tests on 5.7 & 5.8
    
    ---------
    
    Co-authored-by: Moritz Lang <hi@slashmo.codes>

--- Package.swift
+++ Package.swift
@@ -1,17 +1,18 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
-    name: "opentelemetry-swift-xray",
+    name: "swift-otel-xray",
+    platforms: [.macOS(.v13)],
     products: [
         .library(name: "OpenTelemetryXRay", targets: ["OpenTelemetryXRay"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/slashmo/opentelemetry-swift.git", .upToNextMinor(from: "0.1.1")),
+        .package(url: "https://github.com/slashmo/swift-otel.git", .upToNextMinor(from: "0.7.0")),
     ],
     targets: [
         .target(name: "OpenTelemetryXRay", dependencies: [
-            .product(name: "OpenTelemetry", package: "opentelemetry-swift"),
+            .product(name: "OpenTelemetry", package: "swift-otel"),
         ]),
         .testTarget(name: "OpenTelemetryXRayTests", dependencies: [
             .target(name: "OpenTelemetryXRay"),
